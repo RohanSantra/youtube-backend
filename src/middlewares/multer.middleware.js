25964a7c4a8010d11d226be3f08f6e9aef6b250e
Rohan <kajalsantra816@gmail.com>
2025-11-06 18:24:29 +0530

    fix: correct typo in filename function in multer middleware

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -5,7 +5,7 @@ const storage = multer.diskStorage({
     destination: function (req, res, cb) {
         cb(null, "./public/temp")
     },
-    ilename: function (req, file, cb) {
+    filename: function (req, file, cb) {
 
         cb(null, file.originalname)
     }
